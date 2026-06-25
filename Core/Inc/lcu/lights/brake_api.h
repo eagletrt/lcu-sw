1723b865bd3a3c23731a2c180719e3db67d6c5e0
Mario-Mazzara <115786855+Mario-Mazzara@users.noreply.github.com>
2026-06-25 15:35:23 +0200

    chore: docs and return codes #6

--- Core/Inc/lcu/lights/brake_api.h
+++ Core/Inc/lcu/lights/brake_api.h
@@ -1,13 +1,58 @@
+/*!
+ * \file brake_api.h
+ * \date 2026-06-25
+ * \authors Mario Mazzara [mario.mazzara@eagletrt.it] 
+ *
+ * \brief A simple api to turn the brake on and off.
+ *
+ * \details This library provides a way to controll the brake status in an hardware agnostic way
+ *
+ */
 #ifndef BRAKE_API_H
 #define BRAKE_API_H
 
-//TODO: implement return codes
-
+/*!
+ * \brief           A structure that encapsulates data and functions required to
+ *                  handle brake management.
+ */
 struct brake_api_data {
-    bool brake_status;
-    void (*brake_hw_update)(bool status);
+    bool brake_status;                    /*!< brake status false= off true= on. */
+    void (*brake_hw_update)(bool status); /*!< Function pointer to brake hardware implementation. */
 };
-bool brake_api_init(const struct brake_api_data *init_data);
+
+/*!
+ * \brief           Enumeration with all possible return codes of the library.
+ */
+enum BrakeReturnCode {
+    BRAKE_RC_OK,               /*!< Everything is fine. */
+    BRAKE_RC_INVALID_ARGUMENT, /*!< Invalid parameter data. */
+    BRAKE_RC_NULL_POINTER,     /*!< Unexpected NULL pointer. */
+};
+
+/*!
+ * \brief           Initialize the brake_data.
+ *
+ * \param[in]       init_data: pointer to initialization data.
+ *
+ * \retval          PAL_RC_OK on success, an error otherwise:
+ * \retval          PAL_RC_NULL_POINTER if any of the pointer parameters are NULL.
+ */
+enum BrakeReturnCode brake_api_init(const struct brake_api_data *init_data);
+
+/*!
+ * \brief           set brake status.
+ *
+ * \param[in]       status: false = off true = on.
+ *
+ */
 void brake_api_set_status(bool status);
+
+/*!
+ * \brief           get brake status.
+ *
+ * \retval          false = off 
+ * \retval          true = on.
+ *
+ */
 bool brake_api_get_status();
 #endif // !BRAKE_API_H
--- Core/Src/lcu/lights/brake_api.c
+++ Core/Src/lcu/lights/brake_api.c
@@ -1,3 +1,13 @@
+/*!
+ * \file brake_api.c
+ * \date 2026-06-25
+ * \authors Mario Mazzara [mario.mazzara@eagletrt.it] 
+ *
+ * \brief A simple api to turn the brake on and off.
+ *
+ * \details This library provides a way to controll the brake status in an hardware agnostic way
+ *
+ */
 #include "brake_api.h"
 #include "eagletrt.h"
 #include <stddef.h>
@@ -5,13 +15,13 @@ EAGLETRT_STATIC struct brake_api_data api_data;
 
 //TODO: use return codes
 
-bool brake_api_init(const struct brake_api_data *init_data) {
+enum BrakeReturnCode brake_api_init(const struct brake_api_data *init_data) {
     if (init_data == NULL) {
-        return false;
+        return BRAKE_RC_NULL_POINTER;
     }
     api_data.brake_status = init_data->brake_status;
     api_data.brake_hw_update = init_data->brake_hw_update;
-    return true;
+    return BRAKE_RC_OK;
 }
 
 bool brake_api_get_status() {
